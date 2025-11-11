eabd885d9fc40e4d822b3523ee4e87c872fdc183
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-11-11 14:10:52 +0330

    structure updated, src folder, fonts and localFont added

---
+++ src/constants/localFont.js
@@ -0,0 +1,41 @@
+import localFont from "next/font/local";
+
+const yekanFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/YekanBakh-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/YekanBakh-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/YekanBakh-Light.woff",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/YekanBakh-Heavy.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/YekanBakh-Fat.woff",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/YekanBakh-Bold.woff",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-yekan",
+  style: "normal",
+  display: "block",
+});
+
+export default yekanFont;
