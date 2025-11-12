ab3ae385f78e7c4b4a149127b9d3df1149890c33
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-11-12 09:57:14 +0330

    fonts fixed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,8 +3,6 @@ import Slider from "./_components/Slider/Slider";
 export default function Home() {
   return (
     <div>
-      <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت</p>
-      <p>فونت یاب یک موتور جستجوی ...</p>
       <Slider />
     </div>
   );
--- src/constants/localFont.js
+++ src/constants/localFont.js
@@ -3,32 +3,32 @@ import localFont from "next/font/local";
 const yekanFont = localFont({
   src: [
     {
-      path: "../../public/fonts/YekanBakh-Regular.woff",
+      path: "../fonts/YekanBakh-Regular.woff",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../public/fonts/YekanBakh-Medium.woff",
+      path: "../fonts/YekanBakh-Medium.woff",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/YekanBakh-Light.woff",
+      path: "../fonts/YekanBakh-Light.woff",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../../public/fonts/YekanBakh-Heavy.woff",
+      path: "../fonts/YekanBakh-Heavy.woff",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../public/fonts/YekanBakh-Fat.woff",
+      path: "../fonts/YekanBakh-Fat.woff",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../../public/fonts/YekanBakh-Bold.woff",
+      path: "../fonts/YekanBakh-Bold.woff",
       weight: "900",
       style: "normal",
     },
