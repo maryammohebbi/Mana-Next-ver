a359ed0b736501123bb3c3b278b11e1bb212dc7c
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-11-15 15:16:28 +0330

    about us added

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,35 @@
+import Title from "@/ui/Title";
+import Image from "next/image";
 import React from "react";
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div>
+      <Title title="درباره ما" />
+      <div className="flex border border-secondary w-full h-auto lg:h-[200px]">
+        <div className="w-[700px] bg-white items-center justify-center hidden lg:flex">
+          <Image
+            src="/images/logo.png"
+            width={500}
+            height={500}
+            className="w-52 h-24"
+          />
+        </div>
+        <div className="p-5 flex items-center justify-center">
+          <p className="text-secondary text-center">
+            شرکت اندیشه هوشمند مانا، یکی از شرکت‌های دانش‌بنیان معتبر و مورد
+            تایید سامانه ارزیابی صلاحیت شرکت‌های دانش‌بنیان است. این شرکت در
+            کارگروه اطلاعات مکانی وزارت نیرو عضو بوده و همچنین در سندیکای
+            شرکت‌های شناسایی و مکانیابی رادیویی کشور عضویت دارد. اندیشه هوشمند
+            مانا با مشارکت در پروژه‌های مختلف، به ارائه خدمات نوین در حوزه
+            فناوری اطلاعات و ارتباطات پرداخته و در تولید نرم‌افزار، خدمات
+            مشاوره‌ای و پیمانکاری فعالیت می‌کند. هدف این شرکت ارتقای سطح فناوری
+            و ارائه راهکارهای کارآمد به سازمان‌ها و صنایع مختلف است.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,13 @@ function Header() {
       <div className="flex justify-between w-full h-full max-w-sm sm:max-w-2xl lg:max-w-7xl mx-auto p-2">
         <div className="w-56 h-[90%] bg-green-100"> M E N U</div>
         <div className="h-[90%] flex items-center justify-center">
-          <Image src="/images/logo.png" width={120} height={70} alt="ohm" />
+          <Image
+            src="/images/logo.png"
+            width={1000}
+            height={1000}
+            alt="ohm"
+            className="w-28 h-14"
+          />
         </div>
       </div>
     </div>
