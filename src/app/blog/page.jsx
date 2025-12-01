a262f31f0a3315a31a7eb8b560d8f258df5ea9b9
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-12-01 10:12:36 +0330

    minor style changes

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 function Blog() {
   return (
-    <div className="p-5 max-w-sm md:max-w-lg lg:max-w-2xl bg-tertiary mx-auto h-screen">
+    <div className="p-5 max-w-sm md:max-w-lg lg:max-w-2xl bg-tertiary mx-auto h-screen rounded-lg shadow-2xl">
       <h3 className="font-bold mb-10 text-2xl text-secondary">بلاگ</h3>
       <div className="flex flex-col gap-y-7">
         {posts.map((post) => (
           <Link
             href={`/blog/${post.id}`}
             key={post.id}
-            className="border border-secondary p-2 rounded-sm shadow-xl"
+            className="border border-secondary p-2 rounded-xl shadow-xl"
           >
             <h3 className="font-bold text-lg text-secondary">{post.title}</h3>
             <p className="text-secondary text-sm mb-2">{post.briefText}</p>
