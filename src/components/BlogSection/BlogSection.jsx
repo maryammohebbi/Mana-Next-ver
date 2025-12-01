c68c2b21bb63fa4b70f70544661d8d614893bb01
maryammohebbi <maryam.mohebbi.jr@gmail.com>
2025-12-01 10:09:38 +0330

    see more added to blog section

--- src/components/BlogSection/BlogSection.jsx
+++ src/components/BlogSection/BlogSection.jsx
@@ -2,6 +2,8 @@ import Title from "@/ui/Title";
 import React from "react";
 import PostCard from "./PostCard";
 import { posts } from "@/data/data";
+import Link from "next/link";
+import { BiLeftArrow } from "react-icons/bi";
 
 function BlogSection() {
   return (
@@ -14,6 +16,13 @@ function BlogSection() {
         {posts.slice(-2).map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
+        <Link
+          href={`/blog`}
+          className="text-secondary text-xs w-20 flex gap-x-0.5 items-center"
+        >
+          دیدن همه
+          <BiLeftArrow />
+        </Link>
       </div>
     </div>
   );
